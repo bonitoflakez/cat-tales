ba8651a3a6e62200d96ecf849fc766b85ff74021
bonitoflakez <nikhildhiman9050@gmail.com>
2023-09-05 00:41:27 +0530

    tested sample data on dashboard page

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,8 +1,27 @@
+import { userData, inventoryData, additionalData } from "../utils/sampleData";
+
 export default function Dashboard() {
   return (
     <>
-      <h1>Dashboard</h1>
-      <p className="font-bold">This is a testing page.</p>
+      <div className="dashboard-container">
+        <div className="container m-4 dashboard-user-info">
+          <p className="user-name">username: {userData.uname}</p>
+          <p className="user-xp">xp: {userData.xp}</p>
+          <p className="user-level">level: {userData.level}</p>
+        </div>
+        <div className="container m-4  dashboard-inventory-info">
+          <p className="user-cats">total cats: {inventoryData.cats}</p>
+          <p className="user-items">total items: {inventoryData.items}</p>
+        </div>
+        <div className="container m-4 dashboard-additional-details">
+          <p className="next-drop-in">
+            next drop in: {additionalData.nextDropIn}
+          </p>
+          <p className="user-xp">
+            daily reward claimed: {additionalData.claimedToday}
+          </p>
+        </div>
+      </div>
     </>
   );
 }
---
+++ frontend/src/utils/sampleData.js
@@ -0,0 +1,16 @@
+export const userData = {
+  uname: "Neko chan",
+  xp: 500,
+  level: 5,
+};
+
+export const inventoryData = {
+  cats: 10,
+  items: 10,
+  // arrange properly
+};
+
+export const additionalData = {
+  nextDropIn: "13hr",
+  claimedToday: "true",
+};
